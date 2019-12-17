8e9e395f097c11a7d171664a8f6bf2b010a5042e
Jeff Lapp <lappjeff20@gmail.com>
2019-12-17 14:07:56 -0500

    Set up acceptable icons and display for nav

--- portfolio/src/components/SocialIcons.js
+++ portfolio/src/components/SocialIcons.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { NavLink } from "reactstrap";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SocialIcons = props => {
 	return (
-		<div>
-			<FontAwesomeIcon icon={faGithub} />
-			<FontAwesomeIcon icon={faLinkedin} />
+		<div className="d-flex justify-content-end align-items-end h-100">
+			<NavLink href="https://github.com/lappjeff " className="py-0">
+				<FontAwesomeIcon icon={faGithub} size="2x" />
+			</NavLink>
+			<NavLink
+				href="https://www.linkedin.com/in/jeffrey-lapp-622298181/"
+				className="py-0"
+			>
+				<FontAwesomeIcon icon={faLinkedin} size="2x" />
+			</NavLink>
 		</div>
 	);
 };
