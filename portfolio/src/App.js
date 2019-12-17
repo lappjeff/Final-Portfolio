794f8f83643b9edde7c4e7bd4918014628a57686
Jeff Lapp <lappjeff20@gmail.com>
2019-12-17 11:50:47 -0500

    Started work on NavBar, created index file for exports

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import { NavBar } from "./components";
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<NavBar />
+		</div>
+	);
 }
 
 export default App;
