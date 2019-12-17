fade4fabb5ee3510ade0a111ad42a5cfac10ad7e
Jeff Lapp <lappjeff20@gmail.com>
2019-12-17 12:55:27 -0500

    Added border and shadow to Navbar

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Button, Col, Row } from "reactstrap";
 const NavBar = props => {
 	return (
-		<Row className="mt-2 p-1  border-bottom">
+		<Row className="mt-2 py-1  border-bottom align-items-center shadow-sm ">
 			<Col className="text-center">
-				<h2 className=" font-weight-light">Jeffrey M. Lapp</h2>
+				<h3 className=" font-weight-normal m-0">Jeffrey M. Lapp</h3>
 			</Col>
 			<Col className="text-center">
 				<Button color="primary">Contact</Button>
