5a9ec07531dd8571f4ec2a7b0d16772a4d4c28ab
Jeff Lapp <lappjeff20@gmail.com>
2019-12-17 13:33:01 -0500

    Refactored NavBar to use Nav component

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Button, Col, Row } from "reactstrap";
+import { Button, Nav, NavItem } from "reactstrap";
 const NavBar = props => {
 	return (
-		<Row className="mt-2 py-1  border-bottom align-items-center shadow-sm ">
-			<Col className="text-center">
-				<h3 className=" font-weight-normal m-0">Jeffrey M. Lapp</h3>
-			</Col>
-			<Col className="text-center">
-				<Button color="primary">Contact</Button>
-			</Col>
-		</Row>
+		<Nav fill className="py-2 sticky-top">
+			<NavItem>
+				<h3 className="m-0">Jeffrey M. Lapp</h3>
+			</NavItem>
+			<NavItem>
+				<Button color="primary">Contact Me</Button>
+			</NavItem>
+		</Nav>
 	);
 };
 
