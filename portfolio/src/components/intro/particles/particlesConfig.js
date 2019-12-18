44cc259d5570f37e4c2c541eb06c576b1dea2791
Jeff Lapp <lappjeff20@gmail.com>
2019-12-18 11:41:34 -0500

    Created particles config

---
+++ portfolio/src/components/intro/particles/particlesConfig.js
@@ -0,0 +1,28 @@
+export const params = {
+	particles: {
+		number: {
+			value: 115
+		},
+		size: {
+			value: 2
+		},
+		color: {
+			value: "#000000"
+		},
+		line_linked: {
+			color: "#000000",
+			opacity: 0.08
+		},
+		move: {
+			attract_enable: true
+		}
+	},
+	interactivity: {
+		events: {
+			onhover: {
+				enable: true,
+				mode: "repulse"
+			}
+		}
+	}
+};
