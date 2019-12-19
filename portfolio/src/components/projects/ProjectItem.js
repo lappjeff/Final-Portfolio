e182d2263ef87a4232754874db7a2db3f6b2ed3b
Jeff Lapp <lappjeff20@gmail.com>
2019-12-18 19:13:45 -0500

    Created skeleton components for projects carouself

---
+++ portfolio/src/components/projects/ProjectItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { CarouselItem, CarouselCaption } from "reactstrap";
+
+const ProjectItem = ({ item }) => {
+	return null;
+};
+
+export default ProjectItem;
---
+++ portfolio/src/components/projects/ProjectsCarousel.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Carousel, CarouselControl, CarouselIndicators } from "reactstrap";
+import ProjectItem from "./ProjectItem";
+
+const items = [
+	{ src: "../../assets/me.jpeg", altText: "Slide 1" },
+	{ src: "../../assets/me.jpeg", altText: "Slide 1" },
+	{ src: "../../assets/me.jpeg", altText: "Slide 1" }
+];
+
+const ProjectsCarousel = () => {
+	return <h1>Carousel</h1>;
+};
+
+export default ProjectsCarousel;
