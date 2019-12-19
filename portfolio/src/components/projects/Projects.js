0c437af460a1c8612132c49ff6ccac721d481d5c
Jeff Lapp <lappjeff20@gmail.com>
2019-12-18 19:10:54 -0500

    Made projects page 100% height

--- portfolio/src/components/projects/Projects.js
+++ portfolio/src/components/projects/Projects.js
@@ -4,7 +4,7 @@ import ProjectsCarousel from "./ProjectsCarousel";
 
 const Projects = () => {
 	return (
-		<Row id="projects">
+		<Row id="projects" className=" h-100">
 			<Col>
 				<ProjectsCarousel />
 			</Col>
