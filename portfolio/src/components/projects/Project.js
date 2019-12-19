e6230f79d517c093f06a1a39c94641013ec320c0
Jeff Lapp <lappjeff20@gmail.com>
2019-12-19 14:58:30 -0500

    Started work on project component, again

---
+++ portfolio/src/components/projects/Project.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Project = ({ info }) => {
+	return null;
+};
+
+export default Project;
--- portfolio/src/components/projects/Projects.js
+++ portfolio/src/components/projects/Projects.js
@@ -1,22 +1,11 @@
 import React from "react";
+import Project from "./Project";
 import { Row, Col } from "reactstrap";
 
 const Projects = () => {
 	return (
-		<section className=" vh-100 bg-light">
-			<Row id="projects" className="h-25">
-				<Col className="text-center mt-5 pt-5 ">
-					<h1 className="font-weight-normal">Projects</h1>
-				</Col>
-			</Row>
-			<Row className="h-75 pb-3">
-				<Col className="text-center mt-5 ">
-					<Row className="h-100">
-						<Col className="bg-white mx-3 shadow ">Left side</Col>
-						<Col className="bg-white  mx-3 shadow">Right side</Col>
-					</Row>
-				</Col>
-			</Row>
+		<section className=" bg-light text-center">
+			<h1 id="projects">Projects</h1>
 		</section>
 	);
 };
