e389309bcc9f294218dbb4a17bb8a168780dc1e2
Jeff Lapp <lappjeff20@gmail.com>
2019-12-19 13:18:49 -0500

    Removed carousel, started work on two-column project layout

--- portfolio/src/components/projects/Projects.js
+++ portfolio/src/components/projects/Projects.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Row, Col } from "reactstrap";
-import ProjectsCarousel from "./ProjectsCarousel";
 
 const Projects = () => {
 	return (
-		<Row id="projects" className=" h-100">
-			<Col>
-				<ProjectsCarousel />
+		<Row id="projects" className="bg-white vh-100  align-items-center">
+			<Col className="text-center">
+				<h2>Projects</h2>
+				<Row>
+					<Col>Left side</Col>
+					<Col>Right side</Col>
+				</Row>
 			</Col>
 		</Row>
 	);
